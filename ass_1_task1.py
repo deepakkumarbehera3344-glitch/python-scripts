faeb938f3dabe82820f924c6538cc5113ab28225
Deepak kumar Behera <deepakkumarbehera3344@gmail.com>
2026-01-10 17:19:12 +0530

    Add files via upload

---
+++ ass_1_task1.py
@@ -0,0 +1,13 @@
+a = float(input("Enter a number:"))
+b = float(input("Enter another number:"))
+
+addition = a+b
+subtraction = a-b
+multiplication = a*b
+division = a/b
+
+print("Addition is",addition)
+print("Subtraction is",subtraction)
+print("multiplication is",multiplication)
+print("Division is",division)
+
