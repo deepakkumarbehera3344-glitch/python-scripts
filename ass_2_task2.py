d65d0059c93fe143419bac041e75768c40a26d26
Deepak kumar Behera <deepakkumarbehera3344@gmail.com>
2026-01-10 17:19:30 +0530

    Add files via upload

---
+++ ass_2_task2.py
@@ -0,0 +1,9 @@
+firstname = input("Enter your first name: ")
+lastname = input("Enter your last name: ")
+
+fulname = firstname +" "+lastname
+
+print ("full name:",fulname)
+print ("Hello,i am",fulname,"! welcome to my python program.")
+
+
